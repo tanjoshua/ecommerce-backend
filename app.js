6e9569cb260293bc964c10dafb693633ca939731
tanjoshua <jtanjoshua@gmail.com>
2020-09-02 15:35:09 +0800

    fiddling around with middleware

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
+// imports
 const http = require("http");
 const express = require("express");
 
 // create express request handler
 const app = express();
+
+// adding middleware
+app.use((req, res, next) => {
+  console.log("middleware");
+  next();
+});
+
+app.use((req, res, next) => {
+  res.send("<h1>Hello</h1>");
+});
 
 // create server
 const server = http.createServer(app);
