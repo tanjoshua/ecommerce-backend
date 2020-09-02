6b1ec225cd06c0ac4b78d70937ee70e6eac3511b
tanjoshua <jtanjoshua@gmail.com>
2020-09-02 18:00:12 +0800

    migrate to router

--- app.js
+++ app.js
@@ -2,26 +2,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const mainRoutes = require("./routes/main");
+
 // create express app
 const app = express();
 
 // adding middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-message", (req, res, next) => {
-  res.send(
-    "<form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>send</button></form>"
-  );
-});
-
-app.post("/message", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("home");
-});
+app.use(mainRoutes);
 
 // start server at port 3000
 app.listen(3000);
